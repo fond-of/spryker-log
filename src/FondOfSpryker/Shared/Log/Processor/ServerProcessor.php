424220f01ff9bda6ba77518f36684e0c4f09e42a
Felix Pfister <mail@felix-pfister.de>
2019-01-11 16:47:25 +0100

    - extend spryker server processor to log "HTTP_X_FORWARDED_FOR" parameter - overwrite "createServerProcessorPublic" method to use custom server processor

---
+++ src/FondOfSpryker/Shared/Log/Processor/ServerProcessor.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FondOfSpryker\Shared\Log;
+
+use Spryker\Shared\Log\Processor\ServerProcessor as SprykerServerProcessor;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class ServerProcessor
+ *
+ * @package FondOfSpryker\Shared\Log
+ */
+class ServerProcessor extends SprykerServerProcessor
+{
+    const X_FORWARDED_FOR = 'x_forwarded_for';
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        $serverData = parent::getData();
+
+        $request = Request::createFromGlobals();
+        $serverData[static::X_FORWARDED_FOR] = $request->server->get('HTTP_X_FORWARDED_FOR', null);
+
+        return $serverData;
+    }
+}
